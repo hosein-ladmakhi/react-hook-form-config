521cefcdf83fcede5b8ebc59dc8ca036c230dcee
cody proo <cody20v@gmail.com>
2023-08-26 17:36:37 +0330

    yup validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export type FormTypeValidationValue =
   | { message: string; value: any }
@@ -21,54 +23,88 @@ export interface FormValues {
   gender: string;
 }
 
+const formValidation = yup
+  .object()
+  .shape({
+    username: yup
+      .string()
+      .test(
+        "minLength",
+        "the character must be more than 3 character",
+        (val: any) => val.length >= 3
+      )
+      .required("the username is required"),
+    email: yup
+      .string()
+      .email("email format is incorrect")
+      .required("email is required"),
+    password: yup
+      .string()
+      .test(
+        "minLen",
+        "the password should contain more than 8 character",
+        (val: any) => val.length >= 8
+      )
+      .required("password is required"),
+    isMarried: yup.bool().required("isMarried is required"),
+    gender: yup
+      .string()
+      .test(
+        "checkGender",
+        "enter valid gender",
+        (val: any) => !val || !["Male", "Female"].includes(val)
+      ),
+  })
+  .required();
+
 const forms: FormType[] = [
   {
     name: "username",
     text: "Username",
     id: "username",
     type: "text",
-    rules: {
-      required: {
-        value: true,
-        message: "the username is required",
-      },
-      minLength: {
-        value: 3,
-        message: "your username must contain at least 3 character",
-      },
-    },
+    // rules: {
+    //   required: {
+    //     value: true,
+    //     message: "the username is required",
+    //   },
+    //   minLength: {
+    //     value: 3,
+    //     message: "your username must contain at least 3 character",
+    //   },
+    // },
   },
   {
     name: "email",
     text: "Email Address",
     id: "email",
     type: "email",
-    rules: {
-      required: {
-        value: true,
-        message: "the email address is required",
-      },
-      pattern: {
-        value: /[a-z0-9]+@[a-z]+.[a-z]{2,3}/,
-        message: "email is incorrect format",
-      },
-    },
+    // rules: {
+    //   required: {
+    //     value: true,
+    //     message: "the email address is required",
+    //   },
+    //   pattern: {
+    //     value: /[a-z0-9]+@[a-z]+.[a-z]{2,3}/,
+    //     message: "email is incorrect format",
+    //   },
+    // },
   },
   {
     name: "password",
     text: "Password",
     id: "password",
     type: "password",
-    rules: {
-      required: {
-        value: true,
-        message: "the password is required",
-      },
-      minLength: {
-        value: 8,
-        message: "your password must contain at least 8 character",
-      },
-    },
+    // rules: {
+    //   required: {
+    //     value: true,
+    //     message: "the password is required",
+    //   },
+    //   minLength: {
+    //     value: 8,
+    //     message: "your password must contain at least 8 character",
+    //   },
+    // },
   },
   {
     name: "isMarried",
@@ -79,11 +115,11 @@ const forms: FormType[] = [
   {
     type: "radio",
     name: "gender",
-    rules: {
-      validate: (value: any) => {
-        if (!value) return "enter your gender";
-      },
-    },
+    // rules: {
+    //   validate: (value: any) => {
+    //     if (!value) return "enter your gender";
+    //   },
+    // },
     options: [
       {
         text: "Male",
@@ -101,6 +137,7 @@ export default function App() {
   const { register, formState, handleSubmit } = useForm<FormValues>({
     criteriaMode: "firstError",
     reValidateMode: "onChange",
+    resolver: yupResolver(formValidation) as any,
   });
 
   const onSubmit = (data: FormValues) => {
